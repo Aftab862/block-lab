0591f417cb75e3b2c02612088fbd80435781bc40
Aftab Ahmad <uetianaftab@gmail.com>
2025-06-30 16:01:28 +0500

    service page 3d icons replaced

--- src/data/our-service-data.js
+++ src/data/our-service-data.js
@@ -1,8 +1,20 @@
 
-import service_icon_1 from "@assets/img/services/home-3/icon-1.png";
-import service_icon_2 from "@assets/img/services/home-3/icon-2.png";
-import service_icon_3 from "@assets/img/services/home-3/icon-3.png";
-import service_icon_4 from "@assets/img/services/home-3/icon-4.png";
+import service_icon_1 from "@assets/img/services/home-3/1.png";
+import service_icon_2 from "@assets/img/services/home-3/2.png";
+import service_icon_3 from "@assets/img/services/home-3/3.png";
+import service_icon_4 from "@assets/img/services/home-3/4.png";
+
+
+import service_icon_5 from "@assets/img/services/home-3/5.png";
+import service_icon_6 from "@assets/img/services/home-3/6.png";
+import service_icon_7 from "@assets/img/services/home-3/7.png";
+import service_icon_8 from "@assets/img/services/home-3/8.png";
+
+import service_icon_9 from "@assets/img/services/home-3/9.png";
+import service_icon_10 from "@assets/img/services/home-3/10.png";
+
+
+
 
 const our_service_data = [
   {
@@ -31,37 +43,37 @@ const our_service_data = [
   },
   {
     id: 5,
-    icon: service_icon_1,
+    icon: service_icon_5,
     title: <>Video <br /> Editing</>,
     description: <>Polished, engaging videos with smooth edits and effects.</>
   },
   {
     id: 6,
-    icon: service_icon_2,
+    icon: service_icon_6,
     title: <>Brand <br /> Identity</>,
     description: <>Memorable branding with strong visual identity systems.</>
   },
   {
     id: 7,
-    icon: service_icon_3,
+    icon: service_icon_7,
     title: <>3D <br /> Modeling</>,
     description: <>Realistic 3D models for products, spaces, or animation.</>
   },
   {
     id: 8,
-    icon: service_icon_4,
+    icon: service_icon_8,
     title: <>3D <br /> Rendering</>,
     description: <>High-quality renders with lifelike lighting and textures.</>
   },
   {
     id: 9,
-    icon: service_icon_1,
+    icon: service_icon_9,
     title: <>Graphics <br /> Designing</>,
     description: <>Bold, creative graphics for print, web, and social media.</>
   },
   {
     id: 10,
-    icon: service_icon_2,
+    icon: service_icon_10,
     title: <>Product <br /> Photography</>,
     description: <>Crisp, professional photos that showcase your products.</>
   },
