45859abb036f5e6dc6211980b0e1cbea7b163192
asadjaved <asadJaved982@gmail.com>
2025-07-04 16:04:34 +0500

    zoom effect on other pages

--- src/common/breadcrumb/breadcrumb.jsx
+++ src/common/breadcrumb/breadcrumb.jsx
@@ -1,42 +1,78 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
+import AngleArrow from "@/src/svg/angle-arrow";
+import LineArrow from "@/src/svg/line-arrow";
 import breadcrumb_img from "@assets/img/breadcrumb/titile.svg";
+import { EffectFade, Navigation } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
 
 const Breadcrumb = ({
   top_title = "Our Company",
   page_title = "Our Company",
 }) => {
+
+  const [isLoop, setIsLoop] = useState(false)
+  useEffect(() => {
+    setIsLoop(true)
+  }, [])
+
+  const setting = {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    effect: "fade",
+    autoplay: {
+      delay: 5000,
+    },
+
+  };
+
+
   return (
     <>
       <section
-        className="breadcrumb__area breadcrumb-style pt-190 pb-210 p-relative z-index-1"
-        // style={{
-        //   backgroundImage: top_title ==="Contact Us" ? `url(/assets/img/breadcrumb/contact.jpg`:  `url(/assets/img/breadcrumb/breadcrumb-bg-1.jpg)`,
-        // }}
-
-        style={{
-          backgroundImage: `url(${{
-              "Contact Us": "/assets/img/breadcrumb/contact.jpg",
-              "Our Portfolio": "/assets/img/breadcrumb/portfolio.jpg",
-              "Our Services": "/assets/img/breadcrumb/services.jpg",
-              "about": "/assets/img/breadcrumb/company.jpg",
-            }[top_title] || "/assets/img/breadcrumb/breadcrumb-bg-1.jpg"
-            })`,
-        }}
-
-
+        className="breadcrumb__area breadcrumb-style   p-relative z-index-1"
       >
-        <div className="breadcrumb__bg-overlay m-img"></div>
-        <div className="container">
-          <div className="row justify-content-center">
-            <div className="col-xl-10">
-              <div className="breadcrumb__content text-center">
-                <h3 className="breadcrumb__title text-white">
-                  {top_title}
-                  <Image src={breadcrumb_img} alt="theme-pure" />
-                </h3>
-                {/* <div className="breadcrumb__list breadcrumb__list-translate">
+
+        <div className="hero-active-1 swiper-container">
+          <Swiper {...setting} loop={isLoop} modules={[Navigation, EffectFade]}>
+            <SwiperSlide >
+              <div className="tp-hero-inner-1">
+                <div className="container">
+                  <div className="tp-hero-shape">
+
+                  </div>
+                  <div className="tp-hero-1">
+                    <div className="tp-hero-bg tp-hero-overlay p-relative"
+                      style={{
+
+                        backgroundImage: `url(${{
+                          "Contact Us": "/assets/img/breadcrumb/contact.jpg",
+                          "Our Portfolio": "/assets/img/breadcrumb/portfolio.jpg",
+                          "Our Services": "/assets/img/breadcrumb/services.jpg",
+                          "about": "/assets/img/breadcrumb/company.jpg",
+                        }[top_title] || "/assets/img/breadcrumb/breadcrumb-bg-1.jpg"
+                          })`,
+
+                      }}
+                    ></div>
+                    <div className="row pt-190 pb-210">
+                      <div className="breadcrumb__bg-overlay m-img"></div>
+                      <div className="container">
+                        <div className="row justify-content-center">
+                          <div className="col-xl-10">
+                            <div className="breadcrumb__content swiper-slide  swiper-slide-active  tp-hero-title text-center">
+                              <div className="tp-hero-title-wrapper">
+                                <span className="tp-section-title__pre p-relative text-capitalize">
+
+                                  <h3 className="breadcrumb__title   text-white ">
+                                    {top_title}
+                                    <Image src={breadcrumb_img} alt="theme-pure" />
+                                  </h3>
+
+                                </span>
+                              </div>
+                              {/* <div className="breadcrumb__list breadcrumb__list-translate">
                   <span className="text-white">
                     {" "}
                     <Link href="/">Home</Link>{" "}
@@ -51,10 +87,24 @@ const Breadcrumb = ({
                   </span>
                   <span className="text-white">{page_title}</span>
                 </div> */}
+
+
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+
+                    </div>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
+            </SwiperSlide>
+
+          </Swiper>
         </div>
+
+
+
       </section>
     </>
   );
