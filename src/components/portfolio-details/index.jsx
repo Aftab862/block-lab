cec293926501afea1ef92c316c0aa56ae4f79db4
Aftab <uetianaftab@gmail.com>
2025-07-14 23:26:01 -0700

    production issue fix

--- src/components/portfolio-details/index.jsx
+++ src/components/portfolio-details/index.jsx
@@ -1,7 +1,7 @@
 import Breadcrumb from "@/src/common/breadcrumb/breadcrumb";
 import HeaderOne from "@/src/layout/headers/header";
 import React from "react";
-import PortfolioDetailsArea from "./portfolio-details-area";
+// import PortfolioDetailsArea from "./portfolio-details-area";
 import FooterContact from "@/src/layout/footers/footer-contact";
 import FooterThree from "@/src/layout/footers/footer-3";
 
@@ -11,7 +11,7 @@ const PortfolioDetails = () => {
       <HeaderOne />
       <main>
         <Breadcrumb top_title="IT Advisor" page_title=" Portfolio Details" />
-        <PortfolioDetailsArea />
+        {/* <PortfolioDetailsArea /> */}
         <FooterContact bg_style={true} />
       </main>
       <FooterThree />
